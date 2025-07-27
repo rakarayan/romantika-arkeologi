f29555084b85175c1883784190eb94311c4d8605
rakarayan <ajishahariza27@gmail.com>
2025-07-27 21:01:23 +0700

    Create Editorial.js

---
+++ src/components/Editorial.js
@@ -0,0 +1,13 @@
+function Editorial() {
+    return (
+        <div className="container mx-auto py-8 px-4">
+            <section className="mb-12">
+                <h2 className="text-3xl font-bold text-yellow-600 mb-4">Pengantar Redaksi</h2>
+                <p className="text-lg text-gray-700">
+                    Kajian arkeologi di Indonesia memiliki peran penting dalam memahami sejarah dan budaya masa lampau. Jurnal Romantika Arkeologi hadir untuk mendorong mahasiswa sarjana di seluruh Indonesia berpartisipasi dalam pengembangan ilmu arkeologi melalui publikasi artikel ilmiah. Dengan semangat kolaborasi, kami berharap jurnal ini dapat menjadi wadah diskusi yang memperkaya wawasan pembaca dan memajukan penelitian arkeologi.
+                </p>
+                <p className="text-lg text-gray-700 mt-4">Salam,<br>Tim Redaksi KAMA FIB UI</p>
+            </section>
+        </div>
+    );
+}
