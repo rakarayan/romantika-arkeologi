902a2d8da91c84af46b66eaa609c558bee1e2846
rakarayan <ajishahariza27@gmail.com>
2025-07-27 21:00:11 +0700

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+function Footer() {
+    return (
+        <footer className="bg-blue-900 text-white text-center py-4">
+            <p>&copy; 2025 Keluarga Mahasiswa Arkeologi FIB UI. All Rights Reserved.</p>
+        </footer>
+    );
+}
