50a617b52dbf4d2e47de440f1a9938fe4a32751c
rakarayan <ajishahariza27@gmail.com>
2025-07-27 21:01:00 +0700

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+function About() {
+    return (
+        <div className="container mx-auto py-8 px-4">
+            <section className="mb-12">
+                <h2 className="text-3xl font-bold text-yellow-600 mb-4">Tentang Jurnal</h2>
+                <p className="text-lg text-gray-700">
+                    Jurnal Romantika Arkeologi dikelola oleh Tim Penerbitan Ilmiah Keluarga Mahasiswa Arkeologi (KAMA) Fakultas Ilmu Pengetahuan Budaya Universitas Indonesia, di bawah pengawasan Ketua Program Studi Arkeologi FIB UI. Jurnal ini terbit setiap bulan November dengan tema tertentu, dalam format cetak dan elektronik, untuk memajukan kajian arkeologi di kalangan mahasiswa.
+                </p>
+            </section>
+        </div>
+    );
+}
